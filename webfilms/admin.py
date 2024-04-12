6c1c79752d421f4eea9d39d4614a3fd5c6b7f882
gorzanskik <gorzanskik.ai@gmail.com>
2024-04-12 12:02:29 +0200

    urls, views, templates

--- webfilms/admin.py
+++ webfilms/admin.py
@@ -4,4 +4,12 @@ from .models import Film
 
 # Register your models here.
 
-admin.site.register(Film)
+#admin.site.register(Film)
+@admin.register(Film)
+
+class FilmAdmin(admin.ModelAdmin):
+    #fields = ['title', 'year']
+    #exclude = ['description']
+    #list_display = ['title', 'year', 'imdb_rating']
+    #list_filter = ['imdb_rating', 'year']
+    search_fields = ['title']
--- webfilms/views.py
+++ webfilms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Film
 
 # Create your views here.
+
+def all_films(request):
+    #return HttpResponse('first test')
+    all_films = Film.objects.all()
+    return render(request, 'films.html', {'films': all_films}) #templates
